0e31df4deb7917ac3d34aa5ee0720a7265b842d0
GoodDeveloper-coder <itmancik@gmail.com>
2023-10-14 13:43:38 +0300

    Undo money counter script

--- Mini-JamProject/Assets/GoodDeveloper-coder/Scripts/MoneyScript.cs
+++ Mini-JamProject/Assets/GoodDeveloper-coder/Scripts/MoneyScript.cs
@@ -8,11 +8,11 @@ public class MoneyScript : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI MoneyText1;
     [SerializeField] TextMeshProUGUI MoneyText2;
-    private float money1desplayscore;
-    private float money2desplayscore;
+   // private float money1desplayscore;
+   // private float money2desplayscore;
     public GlobalValues GlobalValues;
 
-    [SerializeField] float transitionSpeed = 100;
+    [SerializeField] float transitionSpeed = 0.1f;
 
     // Start is called before the first frame update
     void Start()
@@ -23,10 +23,10 @@ public class MoneyScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GlobalValues.Money1 += Mathf.MoveTowards(money1desplayscore, GlobalValues.Money1, transitionSpeed * Time.deltaTime);
-        GlobalValues.Money2 += Mathf.MoveTowards(money1desplayscore, GlobalValues.Money1, transitionSpeed * Time.deltaTime);
-        MoneyText1.text = string.Format("Money:{0,00}", GlobalValues.Money1);
-        MoneyText2.text = string.Format("{0:0000}:Money", GlobalValues.Money2);
+        //GlobalValues.Money1 += Mathf.MoveTowards(money1desplayscore, GlobalValues.Money1, transitionSpeed * Time.deltaTime);
+       // GlobalValues.Money2 += Mathf.MoveTowards(money1desplayscore, GlobalValues.Money1, transitionSpeed * Time.deltaTime);
+        MoneyText1.text = string.Format("Money:{0:0}", GlobalValues.Money1);
+        MoneyText2.text = string.Format("{0:0}:Money", GlobalValues.Money2);
 
     }
 
@@ -34,10 +34,10 @@ public class MoneyScript : MonoBehaviour
     {
 
         yield return new WaitForSeconds(1f);
-        money1desplayscore = 1;
-        money2desplayscore = 1;
-        //GlobalValues.Money1 += Mathf.MoveTowards(money1desplayscore, GlobalValues.Money1, transitionSpeed * Time.deltaTime);
-        //GlobalValues.Money2 += 1;
+       // money1desplayscore = 1;
+       // money2desplayscore = 1;
+        GlobalValues.Money1 += 1;
+        GlobalValues.Money2 += 1;
         //MoneyText1.text = string.Format("Money:{00}", GlobalValues.Money1);
         //MoneyText2.text = string.Format("{00}:Money", GlobalValues.Money2);
         StartCoroutine(MoneyTimer());
