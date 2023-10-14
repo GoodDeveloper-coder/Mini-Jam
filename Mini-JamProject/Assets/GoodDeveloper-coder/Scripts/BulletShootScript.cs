bd1a035f3ac9b3275a4ebf8abe1223e54ecb62f6
GoodDeveloper-coder <itmancik@gmail.com>
2023-10-14 16:53:29 +0300

    Remove ShaderGraph & add shoot system

---
+++ Mini-JamProject/Assets/GoodDeveloper-coder/Scripts/BulletShootScript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletShootScript : MonoBehaviour
+{
+    public GameObject BulletPrefab;
+    public GameObject BulletPrefabSpawnPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0)){
+            Instantiate(BulletPrefab, BulletPrefabSpawnPosition.transform.position, Quaternion.identity);
+        }
+        
+    }
+}
