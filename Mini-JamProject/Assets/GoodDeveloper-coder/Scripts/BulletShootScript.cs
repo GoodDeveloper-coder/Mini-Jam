395982c72051168f1c5fb9ac7feb210d77d61c0d
GoodDeveloper-coder <itmancik@gmail.com>
2023-10-15 15:55:33 +0300

    Add bullet shooting reload & digerent key code to shoot to each team

--- Mini-JamProject/Assets/GoodDeveloper-coder/Scripts/BulletShootScript.cs
+++ Mini-JamProject/Assets/GoodDeveloper-coder/Scripts/BulletShootScript.cs
@@ -7,6 +7,10 @@ public class BulletShootScript : MonoBehaviour
     public GameObject BulletPrefab;
     public GameObject BulletPrefabSpawnPosition;
 
+    public GlobalValues GlobalValues;
+
+    public int TeamID;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,9 +20,55 @@ public class BulletShootScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0)){
-            Instantiate(BulletPrefab, BulletPrefabSpawnPosition.transform.position, BulletPrefabSpawnPosition.transform.rotation);
+        if (TeamID == 1)
+        {
+            if (Input.GetKeyDown("r"))
+            {
+                if (TeamID == 1)
+                {
+                    if (GlobalValues.LeftTeamShootedTimes >= 3)
+                    {
+                        StartCoroutine(ShootingReloadTime());
+                    }
+                    else
+                    {
+                        Instantiate(BulletPrefab, BulletPrefabSpawnPosition.transform.position, BulletPrefabSpawnPosition.transform.rotation);
+                        GlobalValues.LeftTeamShootedTimes++;
+                    }
+                }
+            }
         }
-        
+
+        if (TeamID == 2)
+        {
+            if (Input.GetKeyDown(KeyCode.RightShift))
+            {
+                if (GlobalValues.RightTeamShootedTimes >= 3)
+                {
+                    StartCoroutine(ShootingReloadTime());
+                }
+                else
+                {
+                    Instantiate(BulletPrefab, BulletPrefabSpawnPosition.transform.position, BulletPrefabSpawnPosition.transform.rotation);
+                    GlobalValues.RightTeamShootedTimes++;
+                }
+            }
+        }
+    }   
+
+
+    IEnumerator ShootingReloadTime()
+    {
+        yield return new WaitForSeconds(3f);
+
+        if (TeamID == 1)
+        {
+            GlobalValues.LeftTeamShootedTimes = 0;
+        }
+
+        if (TeamID == 2)
+        {
+            GlobalValues.RightTeamShootedTimes = 0;
+        }
     }
 }
--- Mini-JamProject/Assets/GoodDeveloper-coder/Scripts/GlobalValues.cs
+++ Mini-JamProject/Assets/GoodDeveloper-coder/Scripts/GlobalValues.cs
@@ -10,6 +10,9 @@ public class GlobalValues : MonoBehaviour
     public int LeftTeamCounterOfPoints;
     public int RightTeamCounterOfPoints;
 
+    public int LeftTeamShootedTimes;
+    public int RightTeamShootedTimes;
+
     // Start is called before the first frame update
     void Start()
     {
